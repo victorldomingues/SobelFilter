c72b2bfd7ed02077a756f4702ffcb4a321407827
Victor Luiz Domingues <victorl.domingues@hotmail.com>
2017-05-27 19:16:48 -0300

    Set Image and display image preview

--- src/Views/ImageSelectView.java
+++ src/Views/ImageSelectView.java
@@ -56,12 +56,14 @@ public class ImageSelectView extends javax.swing.JFrame {
         SelectImageBtn = new javax.swing.JButton();
         SelectImageLabel = new javax.swing.JLabel();
         AplyFilterBtn = new javax.swing.JButton();
-        AplyFilterLabel = new javax.swing.JLabel();
         BeforeImage = new javax.swing.JLabel();
         AfterImage = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sobel FIlter");
+        setBackground(new java.awt.Color(51, 51, 51));
+        setForeground(java.awt.Color.darkGray);
 
         SelectImageBtn.setText("Escolher Imagem");
         SelectImageBtn.addActionListener(new java.awt.event.ActionListener() {
@@ -72,6 +74,7 @@ public class ImageSelectView extends javax.swing.JFrame {
 
         SelectImageLabel.setText("Selecione uma imagem");
 
+        AplyFilterBtn.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         AplyFilterBtn.setText("Aplicar Filtro");
         AplyFilterBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -79,19 +82,32 @@ public class ImageSelectView extends javax.swing.JFrame {
             }
         });
 
-        AplyFilterLabel.setText("Aplicar Filtro");
-
         BeforeImage.setBackground(new java.awt.Color(204, 204, 204));
+        BeforeImage.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         BeforeImage.setForeground(new java.awt.Color(102, 102, 102));
         BeforeImage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         BeforeImage.setText("Pré visualização (Escolha uma Imagem)");
         BeforeImage.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createCompoundBorder(), javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createCompoundBorder(), null)));
 
         AfterImage.setBackground(new java.awt.Color(204, 204, 204));
+        AfterImage.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         AfterImage.setForeground(new java.awt.Color(102, 102, 102));
         AfterImage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         AfterImage.setText("Resultado Final");
         AfterImage.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 396, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -100,35 +116,37 @@ public class ImageSelectView extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(BeforeImage, javax.swing.GroupLayout.PREFERRED_SIZE, 416, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(AfterImage, javax.swing.GroupLayout.PREFERRED_SIZE, 427, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(AplyFilterBtn)
                     .addComponent(SelectImageLabel)
                     .addComponent(SelectImageBtn)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(AplyFilterLabel)))
-                .addContainerGap(16, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(AplyFilterBtn, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(BeforeImage, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 427, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(AfterImage, javax.swing.GroupLayout.PREFERRED_SIZE, 427, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
+                .addGap(52, 52, 52)
                 .addComponent(SelectImageLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(SelectImageBtn)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(AfterImage, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(BeforeImage, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(29, 29, 29)
-                .addComponent(AplyFilterLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(AplyFilterBtn)
-                .addContainerGap(16, Short.MAX_VALUE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(BeforeImage, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(AfterImage, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(AplyFilterBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(21, Short.MAX_VALUE))
         );
+
+        SelectImageLabel.getAccessibleContext().setAccessibleDescription("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -200,14 +218,12 @@ public class ImageSelectView extends javax.swing.JFrame {
 
     private void SetInvisibleComponents() {
         AplyFilterBtn.setVisible(false);
-        AplyFilterLabel.setVisible(false);
 //        AfterImage.setVisible(false);     
 //        BeforeImage.setVisible(false);
     }
 
     private void SetVisibleComponents() {
         AplyFilterBtn.setVisible(true);
-        AplyFilterLabel.setVisible(true);
     }
 
     private void PaintAfter(ImageModel model) {
@@ -224,9 +240,9 @@ public class ImageSelectView extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel AfterImage;
     private javax.swing.JButton AplyFilterBtn;
-    private javax.swing.JLabel AplyFilterLabel;
     private javax.swing.JLabel BeforeImage;
     private javax.swing.JButton SelectImageBtn;
     private javax.swing.JLabel SelectImageLabel;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
